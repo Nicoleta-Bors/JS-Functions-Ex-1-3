29601736b8799827dd5e92255084933820964642
Bors Nicoleta <nicutabors@gmail.com>
2021-06-30 01:30:55 +0300

    added a shorter method

--- Ex.1/Ex1_Kelvin_Weather/app.js
+++ Ex.1/Ex1_Kelvin_Weather/app.js
@@ -1,11 +1,17 @@
 const Kelvin = 293; //The forecast today is 293 Kelvin.
 let Fahrenheit;
 
-const kelvinToFahrenheit = (Kelvin) => {
-  const Celsius = Kelvin - 273; //from Kelvin in Celsius
-  Fahrenheit = Math.floor(Celsius * (9 / 5) + 32); //from Celsius in Fahrenheit + for getting a decimal number
-  return Fahrenheit;
-};
+//long version
+
+// const kelvinToFahrenheit = (Kelvin) => {
+//   const Celsius = Kelvin - 273; //from Kelvin in Celsius
+//   Fahrenheit = Math.floor(Celsius * (9 / 5) + 32); //from Celsius in Fahrenheit + for getting a decimal number
+//   return Fahrenheit;
+// };
+
+//short version
+
+const kelvinToFahrenheit = (Kelvin) => Math.floor((Kelvin - 273) * (9 / 5) + 32);
 
 Fahrenheit = kelvinToFahrenheit(Kelvin);
 console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
