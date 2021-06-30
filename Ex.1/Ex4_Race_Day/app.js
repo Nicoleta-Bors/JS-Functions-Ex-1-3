ef6b20391c5905d1963889af22e39871c67c75f9
Bors Nicoleta <nicutabors@gmail.com>
2021-06-30 13:27:52 +0300

    added a function named raceTime

--- Ex.1/Ex4_Race_Day/app.js
+++ Ex.1/Ex4_Race_Day/app.js
@@ -1,15 +1,22 @@
-let raceNumber = Math.floor(Math.random() * 1000) ;
-let registeredEarly = false; 
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly = true;
 let runnerAge = prompt('Enter your age: ');
+let time = '';
 
-if( runnerAge >= 18 && registeredEarly){
-  raceNumber += 1000;
-}
+const raceTime = (runnerAge, registeredEarly) => {
+  if (runnerAge >= 18 && registeredEarly) {
+    raceNumber += 1000;
+  }
 
-if ( runnerAge >= 18 && registeredEarly){
-  console.log(`Early adults run  at 9:30 am. Your race number is ${raceNumber}`);
-} else if (runnerAge >= 18 && !registeredEarly) {
-  console.log(`Late adults run at 11:00 am. Your race number is ${raceNumber}`);
-} else {
-  console.log(`Youth registrants run at 12:30 am. Your race number is ${raceNumber}`);
-}
+  if (runnerAge >= 18 && registeredEarly) {
+    time = `Early adults run at 9:30 am. Your race number is ${raceNumber}`;
+  } else if (runnerAge >= 18 && !registeredEarly) {
+    time = `Late adults run at 11:00 am. Your race number is ${raceNumber}`;
+  } else {
+    time = `Youth registrants run at 12:30 am. Your race number is ${raceNumber}`;
+  }
+
+  return time;
+};
+
+console.log(raceTime(runnerAge, registeredEarly));
